305d22baac1df9b37e674477ab4edb54ca5db1c2
MenWic <lamr467@gmail.com>
2025-02-06 17:27:32 -0600

    first commit

---
+++ AyD2.java
@@ -0,0 +1,32 @@
+// Usando TDD escribir una funcion que determine si dos palabras son anagramas.
+
+// Notas:
+// Las entradas son dos palabras
+// La salida/return es un boolean indicando si son anagramas o no.
+
+// Ejemplo 1:
+// Entradas:
+// espada
+// pesada
+// Salida: true
+
+
+// Ejemplo 2:
+// Entradas:
+// hola
+// cola
+// Salida: false
+
+class AyD2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    //constructor
+
+    //funciones
+
+    //Getts y Setts
+
+}
